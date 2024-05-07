28762044d87088751deb323e94b388f538c71347
opiyoxbenji <opiyosbenji@gmail.com>
2024-05-07 01:14:00 +0000

    task 0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python3
 """
-Task 0 - Simple helper function
+script for task 0
 """
 from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
-    takes 2 integer arguments and returns a tuple of size two
+    return a tuple of size two containing a
+    start index and an end index
     """
-    start, end = 0, 0
-    for i in range(page):
-        start = end
-        end += page_size
-
-    return (start, end)
+    starting = (page - 1) * page_size
+    ending = starting + page_size
+    return starting, ending
