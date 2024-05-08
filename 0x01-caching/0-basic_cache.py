e651ac3f898a00eff51daa55ef1b5aac8af06f2a
opiyoxbenji <opiyosbenji@gmail.com>
2024-05-08 11:32:49 +0000

    basic caching

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+Task 0 - Caching system
+"""
+from base_caching import BaseCatching
+
+
+class BasicCache(BasicCaching):
+    """
+    Defines a class for caching information in key-value pairs
+    
+    Methods:
+        put(key, item) - store a key-value pair in the cache
+        get(key) - retrieve the value associated with a given key from the cache
+    """
+
+    def __init__(self):
+        """
+        Initialize the class using the parent class __init__ method
+        """
+        BaseCaching.__init__(self)
+
+    def put(self, key, item):
+        """
+        Store a key-value pair in the cache
+
+        Args:
+            Key: The key for the item to be stored
+            Item: The value to be stored
+        """
+        if key is None or item is None:
+            pass
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Return value linked to key.
+        If key is None or doesn't exist, return None
+        """
+        if key is not None and key in self.cache_data.keys():
+            return self.cache_data[key]
+        return None 
